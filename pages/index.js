1cd8954835711029890227abb2709c8f9b634d47
Admin <subashbaidya9@gmail.com>
2023-04-30 09:55:22 +0545

    Layout Components

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem expedita aut ipsum sit exercitationem, corrupti optio minima, esse et, maiores vero cum deleniti molestiae. Natus quas nulla repellat consectetur fugit.</p>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem expedita aut ipsum sit exercitationem, corrupti optio minima, esse et, maiores vero cum deleniti molestiae. Natus quas nulla repellat consectetur fugit.</p>
       <Link href="/servants">See all Servants</Link>
-      <Footer/>
     </div>
   )
 }
