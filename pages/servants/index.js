8bd7a481a7132a7d68da9ee45b2fffd633badf01
Admin <subashbaidya9@gmail.com>
2023-05-01 11:08:19 +0545

    Fetching Data

--- pages/servants/index.js
+++ pages/servants/index.js
@@ -1,7 +1,18 @@
 import Head from "next/head";
 import React from "react";
+import styles from '../../styles/Servants.module.css'
+import Link from "next/link";
 
-const Servants = () => {
+export const getStaticProps = async()=>{
+
+  const res = await fetch('https://api.atlasacademy.io/export/NA/nice_servant.json');
+  const data = await res.json();
+  return {
+    props: {servants: data}
+  }
+}
+
+const Servants = ({ servants }) => {
   return (
     <>
       <Head>
@@ -10,6 +21,11 @@ const Servants = () => {
       </Head>
       <div>
         <h1>All Servants</h1>
+        {servants.map((servant)=>(
+          <div key={servant.id}>
+            <Link className={styles.single} href={'/'}><h3>{servant.name}</h3></Link>
+          </div>
+        ))}
       </div>
     </>
   );
